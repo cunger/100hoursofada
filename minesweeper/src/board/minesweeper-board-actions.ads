73e71eca425a13ed15c20deb6ce15f64e064df08
Christina Unger <christina@okapiorbits.com>
2023-10-26 08:35:47 +0200

    Hour 18: add post-conditions

--- minesweeper/src/board/minesweeper-board-actions.ads
+++ minesweeper/src/board/minesweeper-board-actions.ads
@@ -1,9 +1,11 @@
 package Minesweeper.Board.Actions is
 
    procedure Toggle_Flag (C : in out Cell)
-      with Pre => Is_Hidden (C);
+      with Pre  => Is_Hidden (C),
+           Post => Is_Flagged (C) /= Is_Flagged (C'Old);
 
    procedure Reveal (C : in out Cell)
-      with Pre => Is_Hidden (C);
+      with Pre =>  Is_Hidden (C),
+           Post => not Is_Hidden (C);
 
 end Minesweeper.Board.Actions;
