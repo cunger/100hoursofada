6182229f6558acd4378303a5b539816726f1e7f9
Christina Unger <christina@okapiorbits.com>
2023-11-02 08:42:32 +0100

    Hour 22: read and add doc

--- minesweeper/src/board/minesweeper-boards-actions.ads
+++ minesweeper/src/board/minesweeper-boards-actions.ads
@@ -1,10 +1,21 @@
+------------------------------------------------------------
+-- This package defines the actions a user can take
+-- on a given game board.
+------------------------------------------------------------
 package Minesweeper.Boards.Actions is
 
+   -- The user can flag a cell as being mined,
+   -- and can also remove that flag again.
+   -- When a cell is flagged, it cannot be stepped on.
    procedure Toggle_Flag (C : in out Cell)
    with
       Pre  => Is_Hidden (C),
       Post => Is_Flagged (C) /= Is_Flagged (C'Old);
 
+   -- A user can step on a cell, which reveals the cell
+   -- and possibly adjacent parts of the board.
+   -- If the cell contains a mine, it will explode
+   -- (which means game over).
    procedure Reveal (C : in out Cell)
    with
       Pre =>  not Is_Flagged (C),
