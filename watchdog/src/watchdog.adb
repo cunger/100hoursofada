7965a39483d1c762a62f2c7f43192cefab7301ce
christina <andrea.christina.unger@gmail.com>
2023-11-17 08:32:26 +0100

    Hour 30: splitting watchdog into two tasks

--- watchdog/src/watchdog.adb
+++ watchdog/src/watchdog.adb
@@ -1,6 +1,7 @@
+with Ada.Exceptions;
 with Util.Log.Loggers;
 
--- Note: This is work in progress; really just tinkering at the moment.
+-- Note: This is work in progress and more tinkering than proper working code.
 package body Watchdog is
    Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("watchdog");
 
@@ -14,27 +15,25 @@ package body Watchdog is
    ) is
    begin
       Expected_Ping_Interval := Promised_Ping_Interval;
-      Reboot_Watched_Application := Reboot_Callback;
+      Reboot_Application := Reboot_Callback;
 
-      Run.Start;
+      Timer.Start;
    end Start_With;
 
    procedure Ping is
    begin
-      Run.Ping;
+      Timer.Ping;
    end Ping;
 
    -------------------------
    -- Task implementation --
    -------------------------
 
-   task body Run is
-
+   -- The timer task keeps track of when the last ping was received,
+   -- and calls the interface task for a reboot if there is no ping.
+   task body Timer is
       Last_Ping : Time;
-      -- The watchdog keeps track of when it received the last ping.
-
    begin
-      -- First wait for the application to start the watchdog.
       accept Start do
          Log.Debug ("Watching!");
          Last_Ping := Clock;
@@ -48,11 +47,31 @@ package body Watchdog is
                Last_Ping := Clock;
             end Ping;
          or
-            -- If there is no ping within the expected interval, reboot the process.
+            -- If there is no ping within the expected interval, tell the application to reboot.
             delay until (Last_Ping + Expected_Ping_Interval);
-            Log.Error ("Timeout! Rebooting the application...");
-            Reboot_Watched_Application.all;
+            Log.Error ("Timeout!");
+            Interface_To_Application.Reboot;
          end select;
       end loop Wait;
-   end Run;
+   exception
+      when E : Tasking_Error | Program_Error | Constraint_Error =>
+         Log.Error ("Exception: " & Ada.Exceptions.Exception_Message (E));
+   end Timer;
+
+   -- The interface task is responsible for rebooting the application.
+   task body Interface_To_Application is
+   begin
+      loop
+         select
+            accept Reboot do
+               Reboot_Application.all;
+            end Reboot;
+         or
+            terminate;
+         end select;
+      end loop;
+   exception
+      when E : Tasking_Error | Program_Error | Constraint_Error =>
+         Log.Error ("Exception: " & Ada.Exceptions.Exception_Message (E));
+   end Interface_To_Application;
 end Watchdog;
