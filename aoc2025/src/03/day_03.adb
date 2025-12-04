4e4867fd83b233acf23d0fe3e43d0626c9a8d0d3
Christina Unger <christina@okapiorbits.com>
2025-12-04 08:38:19 +0100

    AoC 2025: Day 3 (Part 2)

--- aoc2025/src/03/day_03.adb
+++ aoc2025/src/03/day_03.adb
@@ -5,20 +5,23 @@ package body Day_03 is
 
    package IO renames Ada.Text_IO;
 
-   type Digit is new Natural range 0 .. 9;
-
-   function To_Joltage (D1 : Digit; D2 : Digit) return Natural;
-   function To_Joltage (D1 : Digit; D2 : Digit) return Natural is
+   function Find_Largest_Battery (Bank : Battery_Bank; Start_Index : Natural; End_Index : Natural) return Battery is
+      Max_Battery : Battery;
    begin
-      return Natural'Value (
-         Ada.Strings.Fixed.Trim (D1'Image, Ada.Strings.Left) &
-         Ada.Strings.Fixed.Trim (D2'Image, Ada.Strings.Left)
-      );
-   end To_Joltage;
+      for I in Start_Index .. End_Index loop
+         if I = Start_Index then
+            Max_Battery := Bank (I);
+         elsif Bank (I).Value > Max_Battery.Value then
+            Max_Battery := Bank (I);
+         end if;
+      end loop;
 
-   function Sum_Joltages return Natural is
-      Input : IO.File_Type;
-      Joltages : Natural := 0;
+      return Max_Battery;
+   end Find_Largest_Battery;
+
+   function Sum_Joltages (Number_Of_Batteries : Natural) return Big_Integer is
+      Input    : IO.File_Type;
+      Joltages : Big_Integer := 0;
    begin
       -- Open the input file in read mode.
       IO.Open (File => Input, Mode => IO.In_File, Name => "src/03/input_03.txt");
@@ -27,53 +30,28 @@ package body Day_03 is
       while not IO.End_Of_File (Input) loop
          declare
             Line : constant String := IO.Get_Line (Input);
-            -- Current digit when waling through the line.
-            D : Digit;
-            -- Temporary variables for the two biggest digets.
-            Left_Digit  : Digit := 0;
-            Right_Digit : Digit := 0;
-            -- Maximum Joltage that was achieved so far.
-            Max_Joltage : Natural := 0;
+            Bank : Battery_Bank;
+            -- The battery configuration with largest joltage in that line.
+            Batteries : String (1 .. Number_Of_Batteries);
          begin
+            -- Read line into a battery bank.
             for I in Line'Range loop
-               D := Digit'Value (Line (I .. I));
-
-               -- If either of the digits is not yet set, then do so
-               -- and continue to next one.
-               if Left_Digit = 0 then
-                  Left_Digit := D;
-               elsif Right_Digit = 0 then
-                  Right_Digit := D;
-                  Max_Joltage := To_Joltage (Left_Digit, Right_Digit);
-               else
-                  -- Determine whether one combination increases Max_Joltage:
-                  -- Left_Digit & D, or Right_Digit & D.
-                  declare
-                     Joltage1 : constant Natural := To_Joltage (Left_Digit, D);
-                     Joltage2 : constant Natural := To_Joltage (Right_Digit, D);
-                  begin
-                     if Joltage1 > Max_Joltage and Joltage2 > Max_Joltage then
-                        if Joltage1 >= Joltage2 then
-                           Right_Digit := D;
-                           Max_Joltage := Joltage1;
-                        elsif Joltage2 > Joltage1 then
-                           Left_Digit  := Right_Digit;
-                           Right_Digit := D;
-                           Max_Joltage := Joltage2;
-                        end if;
-                     elsif Joltage1 > Max_Joltage then
-                        Right_Digit := D;
-                        Max_Joltage := Joltage1;
-                     elsif Joltage2 > Max_Joltage then
-                        Left_Digit  := Right_Digit;
-                        Right_Digit := D;
-                        Max_Joltage := Joltage2;
-                     end if;
-                  end;
-               end if;
+               Bank (I) := (I, Digit'Value (Line (I .. I)));
             end loop;
 
-            Joltages := @ + Max_Joltage;
+            -- Pick the largest next battery (leaving enough space at the end of the bank to find as many more as needed).
+            declare
+               Prev_Index : Natural := 0;
+               Next_Battery : Battery;
+            begin
+               for I in 1 .. Number_Of_Batteries loop
+                  Next_Battery  := Find_Largest_Battery (Bank, Prev_Index + 1, Line'Last - Number_Of_Batteries + I);
+                  Batteries (I) := Ada.Strings.Fixed.Trim (Next_Battery.Value'Image, Ada.Strings.Left) (1);
+                  Prev_Index    := Next_Battery.Index;
+               end loop;
+            end;
+
+            Joltages := @ + From_String (Batteries);
          end;
       end loop;
 
@@ -85,8 +63,8 @@ package body Day_03 is
 
    procedure Solve is
    begin
-      IO.Put_Line ("Part 1: " & Sum_Joltages'Image);
-      IO.Put_Line ("Part 2: -");
+      IO.Put_Line ("Part 1: " & Sum_Joltages (2)'Image);
+      IO.Put_Line ("Part 2: " & Sum_Joltages (12)'Image);
    end Solve;
 
 end Day_03;
--- aoc2025/src/03/day_03.ads
+++ aoc2025/src/03/day_03.ads
@@ -1,6 +1,9 @@
 --------------------------------------
 -- https://adventofcode.com/2025/day/3
 --------------------------------------
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use  Ada.Numerics.Big_Numbers.Big_Integers;
+
 package Day_03 is
 
    -- Calculate and print the solution.
@@ -8,7 +11,19 @@ package Day_03 is
 
 private
 
-   -- Solution to part 1.
-   function Sum_Joltages return Natural;
+   type Digit is new Natural range 0 .. 9;
+
+   type Battery is record
+      Index : Positive range 1 .. 100;
+      Value : Digit;
+   end record;
+
+   type Battery_Bank is array (Positive range 1 .. 100) of Battery;
+
+   -- Find the battery with the largest value in the range from start to end index. 
+   function Find_Largest_Battery (Bank : Battery_Bank; Start_Index : Natural; End_Index : Natural) return Battery;
+
+   -- Find the maximum joltage for each line and sum them up.
+   function Sum_Joltages (Number_Of_Batteries : Natural) return Big_Integer;
 
 end Day_03;
